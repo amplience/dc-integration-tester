95859f3713cc2676b96b136fc44c4776568252e1
Réza Kalfane <reza.kalfane@gmail.com>
2023-03-21 10:36:22 +0100

    chore: renamed to category tree

--- src/commands/get-category-tree.ts
+++ src/commands/get-category-tree.ts
@@ -1,17 +1,17 @@
-import { getCommerceAPI, CommonArgs, GetCommerceObjectArgs } from '@amplience/dc-demostore-integration';
+import { getCommerceAPI, CommonArgs } from '@amplience/dc-demostore-integration';
 import { Argv, Arguments } from 'yargs';
 import asyncMethodTester from '../common/async-method-tester';
 import getConfig, { ConfigArgs } from '../common/config';
 import { commonCommandOptions } from '../common/command-options';
 
-export const command = 'get-mega-menu <vendor>';
+export const command = 'get-category-tree <vendor>';
 
-export const desc = 'Gets the mega menu from the given vendor';
+export const desc = 'Gets the category tree from the given vendor';
 
 /**
  * TODO
  */
-type GetMegaMenuArgs = CommonArgs & ConfigArgs
+type GetCategoryTreeArgs = CommonArgs & ConfigArgs
 
 /**
  * TODO
@@ -31,7 +31,7 @@ export const builder = (yargs: Argv): void => {
  * @param argv 
  * @returns 
  */
-export const handler = async (argv: Arguments<GetMegaMenuArgs>): Promise<void> => {
+export const handler = async (argv: Arguments<GetCategoryTreeArgs>): Promise<void> => {
     const config = await getConfig(argv.vendor);
 
     if (config === undefined) {
@@ -40,5 +40,5 @@ export const handler = async (argv: Arguments<GetMegaMenuArgs>): Promise<void> =
 
     const api = await getCommerceAPI(config);
 
-    await asyncMethodTester(api.getMegaMenu.bind(api), `getMegaMenu (${argv.vendor})`, argv);
+    await asyncMethodTester(api.getCategoryTree.bind(api), `getCategoryTree (${argv.vendor})`, argv);
 };
